1072bc0464fde0d8901b18f2d3ca27f8620b4a3b
junhyuk000 <junhyuk000@gmail.com>
2025-02-09 19:37:50 +0900

    chromedriver위치 변경

--- project/Total_Employment_site/site.py
+++ project/Total_Employment_site/site.py
@@ -11,7 +11,7 @@ import urllib
 ### 변경부분
 # ChromeDriver 경로 설정
 # 리눅스 환경 
-CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver" 
+CHROMEDRIVER_PATH = "/usr/bin/chromedriver" 
 # Windows 환경일 경우
 # CHROMEDRIVER_PATH = r"C:\junhyuk\chromedriver-win64\chromedriver.exe"  # ChromeDriver 설치 경로 확인 후 수정
 
