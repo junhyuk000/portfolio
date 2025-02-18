4eff73eb84bb0a2ed0b639da0643cdac262a3db4
junhyuk000 <junhyuk000@gmail.com>
2025-02-18 15:07:17 +0900

    경로 수정

--- project/MovieAPP/movie.py
+++ project/MovieAPP/movie.py
@@ -82,7 +82,7 @@ def register():
         file = request.files['file']
         
         filename = file.filename if file else None
-        user_image_folder = "/home/junhyuk/flask_app/portfolio/project/MovieAPP/static/user_image"
+        user_image_folder = "/app/static/user_image"
 
         if filename:
             file_path = os.path.join(user_image_folder, filename)
