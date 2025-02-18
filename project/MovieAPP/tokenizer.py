f257ba8a3c7734482e95d9011dd8d70aec63c8d9
junhyuk000 <junhyuk000@gmail.com>
2025-02-18 20:26:49 +0900

    뭐지

--- project/MovieAPP/tokenizer.py
+++ project/MovieAPP/tokenizer.py
@@ -1,5 +1,4 @@
 from konlpy.tag import Okt
-import joblib
 
 class TokenizerWrapper:
     def __init__(self):
@@ -8,9 +7,8 @@ class TokenizerWrapper:
     def tokenize(self, text):
         return self.okt.morphs(text)
 
-# 객체 생성
+# ✅ 피클링할 때 Okt 객체를 제외하고 함수만 저장
 tokenizer = TokenizerWrapper()
-
-# ✅ `tokenize` 메서드만 저장 (Okt 객체는 피클링 불가)
 joblib.dump(tokenizer.tokenize, "/app/project/MovieAPP/static/model/tokenizer.pkl")
+
 print("✅ 토크나이저 함수가 성공적으로 저장되었습니다!")
