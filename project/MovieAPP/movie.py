c96b1980d2d77ada17bf8a81fb186c2f58d81e8b
junhyuk000 <junhyuk000@gmail.com>
2025-02-18 12:22:01 +0900

    경로 수정

--- project/MovieAPP/movie.py
+++ project/MovieAPP/movie.py
@@ -82,7 +82,7 @@ def register():
         file = request.files['file']
         
         filename = file.filename if file else None
-        user_image_folder = os.path.join(current_app.root_path, 'static', 'user_image')
+        user_image_folder = os.path.join(current_app.root_path, 'MovieAPP', 'static', 'user_image')
         if filename:
             file_path = os.path.join(user_image_folder, filename)
             file.save(file_path)
