ee9beb2b6500cfc7aaa9503a1aa469a3f79f0902
junhyuk000 <junhyuk000@gmail.com>
2025-02-19 13:24:32 +0900

    수정

--- project/MovieAPP/movie.py
+++ project/MovieAPP/movie.py
@@ -29,20 +29,17 @@ popcornapp = Blueprint('popcornapp', __name__,
 
 manager = DBManager()
 
+# ✅ okt_tokenizer를 globals()에 등록
+globals()["okt_tokenizer"] = okt_tokenizer
 
 # 모델 파일 경로
 tfidf_path = "/app/project/MovieAPP/static/model/tfidf.pkl"
 model_path = "/app/project/MovieAPP/static/model/SA_lr_best.pkl"
 
-# ✅ okt_tokenizer를 globals()에 등록
-globals()["okt_tokenizer"] = okt_tokenizer
 
-with open(tfidf_path, "rb") as tfidf_file:
-    tfidf = joblib.load(tfidf_file)
-
-with open(model_path, "rb") as model_file:
-    model = joblib.load(model_file)
-
+# ✅ `custom_objects` 없이 직접 로드 가능
+tfidf = joblib.load(tfidf_path)
+model = joblib.load(model_path)
 
 
 ### images 폴더 static/images 폴더로 연결
