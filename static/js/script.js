e902965d1c4dbea9bdf5a649f3e75c0e1376dd22
junhyuk000 <junhyuk000@gmail.com>
2025-04-10 15:01:00 +0900

    js 파일 수정

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,17 @@ function toggleFab() {
     const buttons = document.getElementById('fab-buttons');
     buttons.style.display = (buttons.style.display === 'flex') ? 'none' : 'flex';
 }
+function openYoutubePopup() {
+    const width = 800;
+    const height = 450;
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+
+    const url = "https://www.youtube.com/embed/5LbdMNlKQfM?autoplay=1";
+
+    window.open(
+        url,
+        "youtubePopup",
+        `width=${width},height=${height},left=${left},top=${top},resizable=yes`
+    );
+}
