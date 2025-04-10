e74bff360f603e2deeae90c2bad95cb99645c6fc
junhyuk000 <junhyuk000@gmail.com>
2025-04-10 11:22:04 +0900

    js파일 안에서는 url을 사용할수없음

--- static/js/script.js
+++ static/js/script.js
@@ -10,8 +10,3 @@ function toggleFab() {
     const buttons = document.getElementById('fab-buttons');
     buttons.style.display = (buttons.style.display === 'flex') ? 'none' : 'flex';
 }
-
-function openEmailPopup() {
-    window.open("{{ url_for('email') }}", "emailPopup", "width=600,height=700,resizable=yes,scrollbars=yes");
-}
-
