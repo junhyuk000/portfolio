02a4f31d0d1c14788f9c119dc300b8157e80dc51
junhyk000 <junhyk000@gmail.com>
2025-04-11 16:18:57 +0900

    블루프린트 수정

--- project/smart_city/app.py
+++ project/smart_city/app.py
@@ -32,9 +32,9 @@ smartcity = Blueprint(
     'smartcity',
     __name__,
     static_folder=os.path.join(basedir, 'static'),
-    template_folder=os.path.join(basedir, 'templates'),
     url_prefix='/smartcity'
 )
+
 
 # 모듈의 __main__ 네임스페이스에 직접 등록
 sys.modules['__main__'].okt_tokenizer = okt_tokenizer
